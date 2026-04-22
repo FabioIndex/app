9a9c4016a94b70cde3c64a985527088edcd29653
Fabio <134175915+FabioIndex@users.noreply.github.com>
2026-04-22 00:50:11 +0000

    2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 app.get("/", (req, res) => {
-    res.send("bem vindo ao curso ");
+    res.send("bem vindo ao curso tsc watch");
 });
 app.listen(3000, () => {
     console.log("Servidor ativo !!!!!");
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/",(req, res) =>{
-res.send("bem vindo ao curso ")
+res.send("bem vindo ao curso tsc watch")
 })
 
 
